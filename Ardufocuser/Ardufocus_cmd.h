e4b989e153b4cf873cef4558984181e5a8051980
@josemlp91 <josemlp.motefrio@gmail.com>
2015-08-06 00:23:49 +0200

    Refactorizando código antiguo, limpiando y optimizando

---
+++ Ardufocuser/Ardufocus_cmd.h
@@ -0,0 +1,172 @@
+//Definimos los indices de cada comando.
+//OJO: Para añadir un comando debemos asegurarnos que lo añadimos en la posición que corresponde con su indice.
+
+#define AINIT   	0    // Iniciar modo funcionamiento Ardufocuser.
+#define AMODE   	1    // Estableceer modo de funcionamiento.
+#define AG   		2    // Mover enfocador hasta posición pasada como dato.
+#define APOSITION   3    // Solicitar posición actual del enfocador.
+#define ATEMP   	4    // Solicitar temperatura de la lente al enfocador.
+#define ALTEMP   	5    // Solicitar temperatura de la lente al enfocador.
+#define AMICRO   	6    // Establecer multiplicación del micropaso.
+#define AFINE   	7    // Establecer pasos que da el enfocador por cada pulso.
+#define ASPEED   	8    // Establecer la velocidad del movimiento del enfocador.
+#define AACC   		9    // Establecer la aceleración del movimiento del enfocador.
+#define AR   		10   // Fijar posición reletiva del enfocador en un valor personalizado.
+#define AHLIMIT    	11   // Consultar si el enfocador ha llegado a un limite hardware.
+#define	ASLIMIT		12	 // Consultar si el enfocador ha llegado a un limite software.
+#define ASILIMIT   	13   // Establecer  limite software inware.
+#define ASOLIMIT   	14   // Establecer  limite software outware.
+#define AVERS   	15   // Consultar version del firmware.
+#define AMOV   		16   // Consulta si el enfocador esta moviendose.
+#define ARUNA   	17   // Comando para debug: Mueve el motor del enfocador en una dirreción.
+#define ARUNB   	18   // Comando para debug: Mueve el motor del enfocador en una dirreción.
+#define ASTOP   	19   // Comando para debug: Detiene motor del enfocador
+
+
+char *allcmdArdu[20]={
+						"AINIT",
+						"AMODE",
+						"AG",
+						"APOSITION",
+						"ATEMP",
+						"ALTEMP",
+						"AMICRO",
+						"AFINE",
+						"ASPEED",        
+						"AACC",
+						"AR",
+						"AHLIMIT",
+						"ASLIMIT",
+						"ASILIMIT",
+						"ASOLIMIT",
+						"AVERS",
+						"AMOV",
+						"ARUNA",
+						"ARUNB",
+						"ASTOP"
+};
+
+
+// Definición de las funciones asociadas a los comando.
+// Más info de la función junto al define correspondiente.
+
+
+void ardufocuser_command_function_AINIT(){
+	Serial.println("Modo ardufocus"); 
+}
+
+void ardufocuser_command_function_AMODE(){
+	Serial.println("Ajustando modo");  
+}
+
+
+void ardufocuser_command_function_AG(){
+	int Ipar=atoi(data);
+    motor.moveTo(Ipar); 
+    if (DEBUG) { Serial.println(motor.currentposition());  } 
+}
+
+void ardufocuser_command_function_APOSITION(){
+	String posA(motor.currentposition());
+    sendMessageToIndi("APOSITION?"+posA);  
+}
+
+void ardufocuser_command_function_ATEMP(){
+	String atemp(temp_perform);
+    sendMessageToIndi("ALTEMP?"+atemp);   
+}
+
+void ardufocuser_command_function_ALTEMP(){
+	String atemp(temp_perform);
+    sendMessageToIndi("ALTEMP?"+atemp);   
+}
+
+void ardufocuser_command_function_AFINE(){
+	int Ipf=atoi(data);
+    if (Ipf>MAXSTEPPXPULSA) Ipf=MAXSTEPPXPULSA;
+    if (Ipf<1) Ipf=1;
+
+    stepPerPulse=Ipf;
+    String pf(Ipf);
+    hadToReadstepPerPulse=false;
+    sendMessageToIndi("AFINE?"+pf); 
+}
+
+void ardufocuser_command_function_ASPEED(){
+	int Ivel=atoi(data);
+     if (Ivel>MAXVEL) Ivel=MAXVEL;
+     if (Ivel<MINVEL) Ivel=MINVEL;
+     speed=Ivel;
+     String vel(Ivel);
+     hadToReadspeed=false;
+     sendMessageToIndi("ASPEED?"+vel);
+}
+
+
+void ardufocuser_command_function_AACC(){
+	int Iacc=atoi(data);
+     if (Iacc>MAXACC) Iacc=MAXACC;
+     if (Iacc<MINACC) Iacc=MINACC;
+     
+     String acc(Iacc);  
+     motor.setAcceleration(Iacc);
+     sendMessageToIndi("AACC?"+acc);
+}
+
+void ardufocuser_command_function_AR(){
+	int Ir=atoi(data);
+      String r(Ir); 
+      motor.setCurrentposition(Ir);
+      sendMessageToIndi("AR?"+r);
+}
+
+void ardufocuser_command_function_AHLIMIT(){
+	if (limitRunActiveA)sendMessageToIndi("AHLIMIT?I");
+    else if (limitRunSoftwareB) sendMessageToIndi("AHLIMIT?O"); 
+    else sendMessageToIndi("AHLIMIT?"); 
+}
+
+void ardufocuser_command_function_AMOV(){
+	 // Consultar si el enfocador ha llegado a la posicion destino (si se esta moviendo).
+    if (motor.distanceToGo() == 0) { sendMessageToIndi("ASTOP?");} 
+    else {sendMessageToIndi("ARUN?");}
+}
+
+
+void ardufocuser_command_function_AMICRO(){
+	Serial.println("Ajusta micropasos");
+}
+
+void ardufocuser_command_function_AHLIMIT(){
+	Serial.println("Consultar si el enfocador ha llegado a un limite hardware.");
+}
+
+
+void ardufocuser_command_function_ASLIMIT(){
+	Serial.println("Consultar si el enfocador ha llegado a un limite software.");
+}
+
+void ardufocuser_command_function_ASILIMIT(){
+	Serial.println("Establecer limite software inware.");
+}
+
+void ardufocuser_command_function_ASOLIMIT(){
+	Serial.println("Establecer limite software outware. ");
+}
+
+void ardufocuser_command_function_AVERS(){
+	sendMessageToIndi("AVERS?1"); 
+}
+
+
+void ardufocuser_command_function_ARUNA(){
+	 motor.moveTo(30000);
+}
+
+void ardufocuser_command_function_ARUNB(){
+	 motor.moveTo(-30000);
+}
+
+void ardufocuser_command_function_ASTOP(){
+	  motor.stop();
+}
---
+++ Ardufocuser/Ardufocuser.ino
@@ -0,0 +1,596 @@
+
+#define DEBUG 0
+
+#include <TimerOne.h>
+#include <LiquidCrystal.h>
+#include <AccelStepper.h>
+#include "Ardufocus_config.h"
+#include "Ardufocus_cmd.h"
+
+AccelStepper motor(1, PINSTEP, PINDIR);
+
+LiquidCrystal lcd(PINLCD_RS, PINLCD_ENABLE, PINLCD_D4, PINLCD_D5, PINLCD_D6, PINLCD_D7);
+
+
+// Variable para boton activo en cada momento.
+int button=btnNONE;  
+
+// Variables para controlar los limites findes de carrera hardware.
+bool limitRunActiveA=false; bool limitRunActiveB=false;
+
+// Variables para controlar los limites fines de carrera software. 
+bool limitRunSoftwareActiveA=false; bool limitRunSoftwareActiveB=false;
+int  limitRunSoftwareA=300; int  limitRunSoftwareB=-300;
+
+// Variable para controlar velocidad.
+int speed = 0;
+
+// Variable para controlar pasos por cada pulso.
+int stepPerPulse = 0;
+
+// Variable que controla la posición del motor en cada 
+int position;
+
+// Variables para controlar la temperatura.
+int temp=0; int conT=0; float temp_average=0; int temp_perform;   //Temperature in the moment of focus change.
+
+
+// Varaibles para controlar estados pasados del sistema.
+int lastTimeUpdate=1000;
+int lastPulse=btnNONE;
+int lastspeed = 0;
+int laststepPerPulse = 0;
+int lastposition;
+int lastTemp[10];
+int microStep;
+bool motorIsRun;
+
+bool hadToReadspeed=true;
+bool hadToReadstepPerPulse=true;
+long lastTimeReadButton = 0;
+long lastTimeReadController = 0;
+//WorkMode mode;
+
+// Tamaño de los buffer de lectura.              
+const int bSize = 20;
+const int lenghtcommand=10;
+const int lenghtdata=15;
+
+// Parametros para controlar lectura serie.
+int byteCount;
+char buffer[bSize];     // buffer
+char command[bSize];    // almacenamos comando
+char data[bSize];       // almacenamos parametro
+
+
+
+/*
+* Funcion que lee los botones.
+* return boton
+*/
+int read_LCD_buttons(int adc_key){
+
+//Hacer que estos valores se configuren con una rutina  automatica ...
+ if (adc_key > 1000) return btnNONE; 
+ if (adc_key < 50)   return btnRIGHT;  
+ if (adc_key < 250)  return btnUP; 
+ if (adc_key < 450)  return btnDOWN; 
+ if (adc_key < 650)  return btnLEFT; 
+ if (adc_key < 850)  return btnSELECT;  
+ return btnNONE;  
+
+}
+
+
+/*
+* Funcion que imprime en LCD un saludo inicial.
+*/
+void welcome(String msgGreet){
+
+  lcd.begin(16, 2);              
+  lcd.setCursor(0,0);
+  lcd.print(msgGreet);
+  delay(3000);
+  lcd.setCursor(0,0);
+  lcd.print("                ");
+  
+}
+
+/*
+* Funcion que ajusta el brillo de la LCD.
+*/
+void setBrightness(int bright){ analogWrite(PINBRIGHTNESS,bright); }  //Brillo del LCD  
+
+
+/*
+* Rutinas que se ejecutan bajo la interrupción hardware
+* asociada a su corespondiente fin de carrera.
+*/
+void finA(){ limitRunActiveA=true; }
+void finB(){ limitRunActiveB=true; }
+
+
+void timerFunction() { 
+
+  if (!limitRunActiveA and !limitRunActiveB){ 
+  
+      // Controlamos límites hardware para fines de carrera.
+
+      // if (position >= limitRunSoftwareA ){
+      //   limitRunSoftwareActiveA=true;
+      //   if (motor.targetposition <= limitRunSoftwareA)
+      //     motor.stop();
+      //   else motor.run(); limitRunSoftwareActiveA=false;
+      // }
+
+      // if (position <= limitRunSoftwareB ) {
+      //   limitRunSoftwareActiveB=true;
+      //   if (motor.targetposition() >= limitRunSoftwareB)
+      //     motor.stop();
+      //   else motor.run(); limitRunSoftwareActiveB=false;
+      // }
+
+      motor.run();
+   }  ///Necesita testearse muy bien
+
+  else if (limitRunActiveA){
+
+    if (motor.targetposition() < motor.currentposition()){
+      motor.run(); 
+      limitRunActiveA=false;
+
+    } else { motor.stop(); limitRunActiveB=true; }
+  
+  }
+
+  else if (limitRunActiveB){
+
+    if (motor.targetposition() > motor.currentposition()){
+      motor.run();
+      limitRunActiveB=false;
+
+    } else { motor.stop(); limitRunActiveB=true; }
+
+  }
+}
+
+/*
+* Rutinas que actualiza los controles manuale, cuando pulsamos algun boton o actuamos sobre algun potenticometro.
+*/
+void readManualController(){
+
+  // Para evitar sobrecargar la placa hacemos las lecturas dado un intervalo de tiempo.
+
+  // Si ya se a cumplido el intervalo de tiempo, realizamos lectura.
+  if (millis() > lastTimeReadButton + 5) {
+    int adc_key = analogRead(PIN_BUTTON);
+    button = read_LCD_buttons(adc_key);
+    
+    // Refrescamos momento de actualización.
+    lastTimeReadButton = millis();
+  }
+
+  // Si no se cumple intervalo de tiempo, utilizamos el valor anterior.
+  else button=lastPulse;
+
+
+  if (millis() > lastTimeReadController + 200) {
+
+     int s = analogRead(PIN_POTA);
+     // Cuando bajoamos el potenciometro al valor 0 forzamos a actualizar la velocidad.
+     // Esto se hace así para contemplar la posibilidad de que se esten modificando los valores de forma remota.
+     if (s==0) hadToReadspeed=true;
+
+     if (hadToReadspeed){
+        speed = map(s, 0, 1024, MINVEL, MAXVEL ); 
+     }
+
+     // Cuando bajoamos el potenciometro al valor 0 forzamos a actualizar los pasos por pulso.
+     int sS = analogRead(PIN_POTB);
+     if (sS==0) hadToReadstepPerPulse=true; 
+
+     if (hadToReadstepPerPulse){  
+        stepPerPulse = map(sS, 0, 1024, 1, MAXSTEPPXPULSA);
+     }  
+
+     if (abs(motor.speed() - speed ) > 2) {
+       motor.setMaxspeed(speed);
+     }
+
+     // Refrescamos momento de actualización. 
+     lastTimeReadController = millis();
+    }
+}
+
+
+/*
+* Lee otros sensores o perifericos.v 
+*/
+void readOtherSensor(){
+ // Other Sensor
+ //limitRunActiveA=digitalRead(PIN_LIMITRUNA);
+ //limitRunActiveB=digitalRead(PIN_LIMITRUNB);
+ //limitRunActiveA=limitRunActiveB=false;
+  
+}
+
+
+/*
+* Lee la temperatura. 
+*/
+long lastTimeReadTemp = 0;
+float readTemperature(){
+ 
+  if (millis() > lastTimeReadTemp + 10000) {
+
+      float t;
+      t=analogRead(PIN_TEMSENSOR)* 0.48828125;
+      if (abs(t-temp_average)<=2) temp_average=t;
+
+      lastTimeReadTemp = millis();
+    }
+
+  return temp_average;
+
+}
+
+/*
+* Actualiza mensajes que se muestran por pantalla LCD. 
+*/
+long lastTimeUpdateLcd = 0;
+void UpdateLCD(){
+
+
+  // Si cumple el tiempo minimo de refresco.
+  if (millis() > lastTimeUpdateLcd + 500) {
+
+    // Si ha cambiado el boton desde la última pulsación.
+    if (button != lastPulse)  {
+
+      // actualizamos zona LCD donde se muestra el comando.
+      lcd.setCursor(0,1);
+      switch (button){
+        case btnRIGHT:  lcd.print("RIGHT  ");  break;
+        case btnLEFT:   lcd.print("LEFT   ");  break;
+        case btnUP:     lcd.print("RIGHT_F");  break;
+        case btnDOWN:   lcd.print("LEFT_F ");  break;
+        case btnSELECT: lcd.print("OK     ");  break;
+    
+      // Actualizamos boton anterior al boton actual.
+      lastPulse=button;
+
+      }
+
+    }
+
+       // Actualizamos zona de la temperatura
+       lcd.setCursor(2,0);
+       lcd.print("    ");
+       lcd.setCursor(2,0);
+       lcd.print((int)temp_average);
+        
+       // si se produce un cambio significativo en el control de la velocidad.
+       if ((lastspeed>speed+5) or (lastspeed<speed-5))  {
+         // Actualizar zona LCD donde se muestra la velocidad.
+         lcd.setCursor(8,1);
+         lcd.print("    ");
+         lcd.setCursor(8,1);
+         lcd.print(speed);
+         lastspeed=speed;
+       }
+      
+       // Si se produce un cambio significativo en el control de los pasos por pulso.
+       if ((laststepPerPulse>stepPerPulse+5) or (laststepPerPulse<stepPerPulse-5))  {
+         // Actualizar zona LCD donde se muestran los pasos por pulso.
+         lcd.setCursor(12,1);
+         lcd.print("    ");
+         lcd.setCursor(12,1);
+         lcd.print(stepPerPulse);
+         laststepPerPulse=stepPerPulse;
+      }  
+        
+       // Si se produce un cambio significativo en la posicion.
+       if ((lastposition>position+1) or (lastposition<position-1)){
+         // Actualizar zona LCD donde se muestran la posición.
+         lcd.setCursor(4,0);
+         lcd.print("            ");    
+         lcd.setCursor(8,0);
+         lcd.print(position);
+         lastposition=position;
+     }
+
+     // Refrescamos momento de actualizacion.
+     lastTimeUpdateLcd = millis();
+  }      
+
+}
+
+
+// long lastTimeReadTemp = 0;
+// void readTemperature(){
+
+//   if (millis() > lastTimeReadTemp + 100) {
+
+//     lastTemp[conT]= analogRead(PIN_TEMSENSOR) ;
+//     conT++;
+//     if (conT==10){
+//       Serial.println(conT);
+//       Serial.println(temp_average);
+//       for (int i = 0; i<10; ++i){
+//         temp_average+=lastTemp[i];
+        
+
+//       }
+//       temp_average/=10;
+//       temp_average*=0.48828125;
+//       conT=0;
+//     }
+
+//     lastTimeReadTemp = millis();
+//   }
+
+// }
+
+
+
+
+
+bool changeThermalOptical(int temp_average, int temp_perform){
+   if ((temp_average>temp_perform+MAX_CHANGE_TEMP) || (temp_average<temp_perform-MAX_CHANGE_TEMP)) return true;
+   else return false;
+}
+
+void enable_warning() { digitalWrite(PINLED_WARNING,HIGH); }
+void disable_warning(){ digitalWrite(PINLED_WARNING,LOW); }
+
+void manualPerformance(){
+
+  //if (mode!=REMOTE){
+  //motor.setAcceleration(DEF_ACC);   // Esta funcion no se puede ejecutar mucho
+  position=motor.currentposition();
+
+  if (changeThermalOptical(temp_average, temp_perform)) enable_warning();
+      
+  switch (button){
+
+    case btnNONE: {
+
+                    if ((lastPulse==btnLEFT)||(lastPulse==btnRIGHT)){  
+                      motor.stop(); 
+                    }
+                   lastPulse=btnNONE; 
+                   break;
+      
+    }
+
+    case btnRIGHT: {
+                    motor.moveTo(motor.currentposition()-3000);
+                    lastPulse=btnRIGHT; 
+                    break;
+    }
+
+    case btnLEFT:   {
+                      motor.moveTo(motor.currentposition()+3000); 
+                      lastPulse=btnLEFT; 
+                      break;
+    }
+
+    case btnUP:   {
+                    if (lastPulse!=btnUP){
+                        motor.moveTo(motor.currentposition() + stepPerPulse);
+                    }
+                    
+                    lastPulse=btnUP;    
+                    break;
+    }
+
+    case btnDOWN: {
+                    if (lastPulse!=btnDOWN){
+                        motor.moveTo(motor.currentposition() - stepPerPulse);
+                    }
+                    
+                    lastPulse=btnDOWN;  
+                    break;
+    }
+
+    case btnSELECT: {
+                      temp_perform=temp_average;
+                      disable_warning();
+                      motor.setCurrentposition(0);     
+                      break;
+    }
+
+  } 
+
+  button=btnNONE;
+}
+
+
+/*
+* Parseer del comando que se comunica por el puerto serie, 
+* el formato siempre es CMD?DATA    <comando + delimitador + datos>
+*
+* Pasamos como parametro el delimitador.
+*/
+void serialParser(char delimiter) {
+
+ if (Serial.available()){
+   byteCount = -1;
+
+   // Leemos puerto serie hasta completar buffer.
+   for (int x=0;x<bSize;x++){
+      buffer[x]=Serial.read();
+      byteCount++;
+   }
+
+  }
+
+    // Si hemos leido algun dato.
+    if (byteCount  > 0) {
+
+        char c;          
+        int i=0;
+        // Volcamos contenido del buffer en el contenedor del comando, buscando el delimitador.
+        for (i; ((i<byteCount) and (c!= delimiter)) ; i++){
+              c=buffer[i];
+              command[i]=c;
+              
+        }
+
+        //Avanzamos hasta despues  del delimitador.        
+        i++;
+        int a=0;
+         // Volcamos contenido del buffer en el contenedor del dato.  
+        for (i; (i<byteCount) ; i++){
+          c=buffer[i];
+          data[a]=c;
+          a++;
+      }                  
+    }
+
+   //Limpiamos el buffer
+   memset(buffer, 0, bSize);  
+}
+
+
+void confirmPositionGet(int pos){}
+
+
+// Dada la lectura serie, carga el comando y el dato.
+void remoteManager(){
+
+  //if (mode!=MANUAL){ 
+    char  d='?';
+    serialParser(d);
+
+    if (byteCount  > 0) {
+      
+      if      (command[0]=='F'){ /*Implementar*/ }           // Si detectamos comando del repertorio de instrucciones del Robofocus.
+      else if (command[0]=='A'){  driveCommandArdufocus(); } // Si detectamos comando del repertorio de instrucciones del Ardufocuser.
+      else    Serial.println("No comando");                  // No pertenece a ningun repertorio de instrucciones.
+      
+    //En modo debug: Hacemos eco de los comandos que recibe para depurar que se estan tratando correctamente.  
+    if (DEBUG){
+      Serial.print("CMD      : ");
+      Serial.println(command);
+      Serial.print("PAR      : ");
+      Serial.println(data);
+      Serial.flush();                      
+    }       
+  }
+
+  //Limpiamos contenedor del comando. 
+  memset(command, 0, sizeof(command));
+  //Limpiamos contenedor de los datos.
+  memset(data, 0, sizeof(data));
+  // Reseteamos contador de bytes leidos.
+  byteCount=0;                         
+        
+  //}
+
+ }
+
+/*
+* Enviamos comando por pueto serie, destinado a ser leido por el servidor Indi.
+*/
+void sendMessageToIndi(String message){
+
+  int len=message.length();
+  for (int i=len;i<bSize;i++){
+    message+=' ';
+  }
+
+  Serial.println(message);
+  Serial.flush();
+
+} 
+
+/*
+* Maneja comandos Ardofocuser. 
+* Dado el comando y el dato, actua sobre el motor, lcd, sensores o controles segun proceda.
+*/
+void driveCommandArdufocus(){
+
+  if      !(strcmp(command,allcmdArdu[AINIT]))       { ardufocuser_command_function_AINIT();     }
+  else if !(strcmp (command,allcmdArdu[AMODE]))      { ardufocuser_command_function_AMODE();     }
+  else if !(strcmp (command,allcmdArdu[AG]))         { ardufocuser_command_function_AG();        }
+  else if !(strcmp (command,allcmdArdu[APOSITION]))  { ardufocuser_command_function_APOSITION(); }
+  else if !(strcmp (command,allcmdArdu[ATEMP]))      { ardufocuser_command_function_ATEMP();     }
+  else if !(strcmp (command,allcmdArdu[ALTEMP]))     { ardufocuser_command_function_ALTEMP();    }
+  else if !(strcmp (command,allcmdArdu[AFINE]))      { ardufocuser_command_function_AFINE();     }
+  else if !(strcmp (command,allcmdArdu[ASPEED]))     { ardufocuser_command_function_ASPEED();    }
+  else if !(strcmp (command,allcmdArdu[AACC]))       { ardufocuser_command_function_AACC();      }
+  else if !(strcmp (command,allcmdArdu[AR]))         { ardufocuser_command_function_AR();        }
+  else if !(strcmp (command,allcmdArdu[AHLIMIT]))    { ardufocuser_command_function_AHLIMIT();   }
+  else if !(strcmp (command,allcmdArdu[AMICRO]))     { ardufocuser_command_function_AMICRO();    }
+  else if !(strcmp (command,allcmdArdu[ASLIMIT] ))   { ardufocuser_command_function_ASLIMIT();   }
+  else if !(strcmp (command,allcmdArdu[ASILIMIT]))   { ardufocuser_command_function_ASILIMIT();  }
+  else if !(strcmp (command,allcmdArdu[ASOLIMIT]))   { ardufocuser_command_function_ASOLIMIT();  }
+  else if !(strcmp (command,allcmdArdu[AVERS]))      { ardufocuser_command_function_AVERS();     }
+  else if !(strcmp (command,allcmdArdu[AMOV]))       { ardufocuser_command_function_AMOV();      }
+  else if !(strcmp (command,allcmdArdu[ARUNA]))      { ardufocuser_command_function_ARUNA();     }
+  else if !(strcmp (command,allcmdArdu[ARUNB]))      { ardufocuser_command_function_ARUNB();     } 
+  else if !(strcmp (command,allcmdArdu[ASTOP]))      { ardufocuser_command_function_ASTOP()      }
+  
+}
+
+
+long lastCurrentPos= millis();
+void sendCurrentposition(){
+
+  if (millis()> lastCurrentPos + 1000){
+    String r(motor.currentposition());
+    sendMessageToIndi("APOSITION?"+ r); 
+  
+    if (motor.distanceToGo() == 0) { sendMessageToIndi("ASTOP?"); }
+  lastCurrentPos=millis();
+  }
+
+}
+
+void setup() {
+  limitRunSoftwareA=-300;
+  limitRunSoftwareB=300;
+  
+  Serial.begin(9600);
+
+  //Configuracion pines entrada salida.
+  pinMode(PINLED_WARNING, OUTPUT);
+
+  motor.setMaxspeed(200);
+  motor.setAcceleration(1000);
+
+  //Variables auxiliares para  saber cuando actualizar la LCD
+  lastTimeUpdate=millis();
+  lastspeed=speed;
+  laststepPerPulse = stepPerPulse;
+  lastposition= position;
+  temp_average=analogRead(PIN_TEMSENSOR)* 0.48828125;
+
+  //Interrupciones software y hardware.
+  Timer1.initialize(50);
+  Timer1.attachInterrupt( timerFunction );                                         
+//         attachInterrupt ( 0, finA,RISING);   
+//         attachInterrupt ( 1, finB,RISING);
+
+  //Ajustamos el brillo y saludo inicial.
+   setBrightness(50);
+   welcome("  ARDUFOCUSER"); 
+
+}
+
+
+
+void loop(){
+
+  readManualController();
+  readOtherSensor();
+  readTemperature();
+  UpdateLCD();
+  manualPerformance();
+  sendCurrentposition();  
+  remoteManager(); 
+
+}
+ 
+
